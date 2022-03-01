0c29ec335da116d6f8b8ad386281c3bd09b96d09
gmatteo <gmatteo@gmail.com>
2022-03-02 00:17:13 +0100

    Fix typat

--- 2F/job.sh
+++ 2F/job.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
 #SBATCH --partition=batch
-#SBATCH --job-name=1F
+#SBATCH --job-name=2F
 #SBATCH --ntasks=96
-##SBATCH --ntasks=64
 #SBATCH --mem-per-cpu=8000
-#SBATCH --time=0-24:0:0
+#SBATCH --time=0-16:0:0
 #SBATCH --output=queue.qout
 #SBATCH --error=queue.qerr
 
@@ -16,4 +15,4 @@ export OMP_NUM_THREADS=1
 source $HOME/git_repos/abinit_rmms/_intel/modules.sh
 export PATH=$HOME/git_repos/abinit_rmms/_intel/src/98_main/:$PATH
 
-mpirun -n ${SLURM_NTASKS} abinit < 1F-2110.files > run.log 2> run.err
+mpirun -n ${SLURM_NTASKS} abinit < 2F-2110.files > run.log 2> run.err
